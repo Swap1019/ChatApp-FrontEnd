4da0fec7cda68159a52615e4ff6bae47a8f96b90
Kian <kianjafari660@gmail.com>
2025-09-21 22:57:46 +0330

    updated env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 // https://vite.dev/config/
 export default defineConfig({
