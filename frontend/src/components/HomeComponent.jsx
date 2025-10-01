d8c9fe06c97b123227bc0921ec4286f22e0e6f19
Kian <kianjafari660@gmail.com>
2025-10-01 23:20:52 +0330

    merging branches

--- frontend/src/components/HomeComponent.jsx
+++ frontend/src/components/HomeComponent.jsx
@@ -1,16 +1,18 @@
-import { Gear, PersonCircle, Send, ArrowLeft } from 'react-bootstrap-icons';
-import { Link } from "react-router-dom";
-import React from 'react';
+import { PersonCircle, InfoCircle,} from 'react-bootstrap-icons';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { useState, useEffect, useRef } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate,Navigate } from "react-router";
 import "../styles/Home.css";
 import "../styles/Base.css";
+import api from "../api"; 
 
-function HomeComponent({user,conversations,messages,uuid}) {
+function HomeComponent({user,conversations,messages,uuid,fetchmembers}) {
     const [content, setContent] = useState("");
     const [socket, setSocket] = useState("");
     const [chatName, setChatName] = useState("");
     const [chatImg, setChatImg] = useState(null);
+    const [members, setMembers] = useState();
     const [liveMessage, setLiveMessage] = useState(messages || []);
     const messagesEndRef = useRef(null);
     const navigate = useNavigate();
@@ -50,6 +52,19 @@ function HomeComponent({user,conversations,messages,uuid}) {
         messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
     };
 
+    const getmembers = async () => {
+        if (uuid && user) {
+            await api
+                .get(`/chat/${uuid}/members`)
+                .then((res) => res.data)
+                .then((data) => {
+                    setMembers(data.members);
+                    console.log(data)
+                })
+                .catch((err) => alert(err));
+        }
+    }
+
     const sendMessage = () => {
         if (!content.trim()) return;
 
@@ -73,7 +88,6 @@ function HomeComponent({user,conversations,messages,uuid}) {
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
     }, []);
-
 
     useEffect(() => {
         scrollToBottom();
@@ -109,101 +123,209 @@ function HomeComponent({user,conversations,messages,uuid}) {
         return () => ws.close();
     }, [uuid]);
 
-
-
     useEffect(() => {
         scrollToBottom();
     }, [liveMessage]);
 
     return (
-        <div className="chat-container">
-            <div className="sidebar" id="sidebar">
-                <input type="text" placeholder="Search chats..." id="searchInput" />
-                <div className="chat-list">
-                {conversations?.map((conversation) => (
-                    <div
-                    key={conversation?.id}
-                    className="list-group-item chat-item"
-                    onClick={() => handleChatClick(conversation)}
-                    >
-                    <img src={conversation.profile} />
-                    <span>{conversation?.name}</span>
+        <>  
+            {user === null ? (
+                <Navigate to="/" replace />
+            ) : (
+                <>
+                    {/* Group modal */}
+                    <div className="modal fade" id="groupModal" aria-hidden="true" aria-labelledby="groupModal" tabIndex={-1}>
+                        <div className="modal-dialog modal-dialog-scrollable modal-fullscreen-md-down ">
+                            <div className="modal-content theme-gray">
+                                <div className="modal-header">
+                                    <img className="avatar me-3" src={chatImg} alt="Profile" id="chatHeaderImg" style={{width: "60px" ,height:"60px"}} />
+                                    <div className="d-flex flex-column">
+                                        <h1 className="modal-title fs-5">{chatName}</h1>
+                                        <span className="fw-lighter text-white">{members?.length} members</span>
+                                    </div>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                
+                                <div className="modal-body">
+                                    {members?.map((member) => (
+                                        <div data-bs-target={`#${member.id}`} data-bs-toggle="modal">
+                                            <div className="row p-3 user-row">
+                                                <div className="col user">
+                                                    {member.profile ? (
+                                                        <img className="avatar me-3" src={member.profile} alt="Profile" style={{width: "50px" ,height:"50px"}} />
+                                                    ) : (
+                                                        <PersonCircle className="avatar me-3" style={{width: "50px" ,height:"50px"}} />
+                                                    )}
+                                                    <span className="fw-bold" style={{fontSize: "18px"}}>{member.nickname}</span>
+                                                </div>
+                                            </div>
+                                        </div>     
+                                    ))}                      
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                ))}
-                </div>
-            </div>
+                    
+                    {/* User modal */}
+                    {members?.map((member) => (
+                        <div className="modal fade " id={`${member.id}`} aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabIndex={-1} key={member.id}>
+                            <div className="modal-dialog modal-dialog-centered modal-fullscreen-md-down">
+                                <div className="modal-content theme-gray">
+                                    <div className="modal-header">
+                                        <div data-bs-target="#groupModal" data-bs-toggle="modal" className="me-2 user-modal-back-button">←</div>
+                                        <h1 className="modal-title fs-5" id="exampleModalToggleLabel2">        
+                                            {member.profile ? (
+                                                <img className="avatar me-3" src={member.profile} alt="Profile" style={{width: "60px" ,height:"60px"}} />
+                                            ) : (
+                                                <PersonCircle className="avatar me-3" style={{width: "50px" ,height:"50px"}} />
+                                            )}
+                                            <span className="fw-bold" style={{fontSize: "18px"}}>{member.nickname}</span>
+                                        </h1>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <div class="container">
+                                            <div class="row">
+                                                <div class="col-1 me-4">
+                                                    <InfoCircle style={{width:"30px" ,height:"30px"}}/>
+                                                </div>
+                                                <div class="col-8">
+                                                    <span className="fw-normal">
+                                                        {member.bio}
+                                                    </span>
+                                                    <p className="fw-ligher text-secondary">
+                                                        bio
+                                                    </p>
+                                                    <span className="fw-normal">
+                                                        @{member.username}
+                                                    </span>
+                                                    <p className="fw-ligher text-secondary">
+                                                        username
+                                                    </p>
+                                                </div>
 
-            {/* Chat content */}
-            <div className="chat-content" id="chatContent" style={{ backgroundImage: `url(${user?.background_image ? user.background_image : "https://res.cloudinary.com/dwfngrwoe/image/upload/v1758392790/default_hgh8gm.webp"})`}}>
-                {uuid ? 
-                <div className="chat-header">
-                    <button id="backBtn" onClick={backToChats}>←</button>
-                    <img src={chatImg} alt="Profile" id="chatHeaderImg" />
-                    <span id="chatName">{chatName}</span>
-                </div>
-                :
-                <div/>
-                }
-                <div className="messages">
-                    {liveMessage?.map((message) => (
-                        user.id === message.sender.id ?
-                            <div className="message sent">
-                                {message.sender.profile ? (
-                                    <img src={message.sender.profile} className="avatar" />
-                                ) : (
-                                    <PersonCircle className="avatar" />
-                                )}
-                                <div className="message-bubble">
-                                    <div className="message-header">
-                                        <span className="nickname">{message?.sender.nickname}</span>
-                                        <span className="time">{prettyDate(message?.created_at)}</span>
+                                                <div class="col-8 ms-5 ">
+                                                    <div className="send-message p-2">
+                                                        Send Message
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className="message-content">
-                                        {message?.content}
+
+                                    <div className="modal-footer">
+                                        <div className="add-to-contacts p-2">
+                                            Add To Contacts
+                                        </div>
+                                        <div className="user-block p-2">
+                                            Block User
+                                        </div>
                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+
+
+                    {/* Main Page */}
+                    <div className="chat-container">
+                        <div className="sidebar" id="sidebar">
+                            <input type="text" placeholder="Search chats..." id="searchInput" />
+                            <div className="chat-list">
+                            {conversations?.map((conversation) => (
+                                <div
+                                key={conversation?.id}
+                                className="list-group-item chat-item"
+                                onClick={() => handleChatClick(conversation)}
+                                >
+                                <img src={conversation.profile} />
+                                <span>{conversation?.name}</span>
+                                </div>
+                            ))}
+                            </div>
+                        </div>
+
+                        {/* Chat content */}
+                        <div className="chat-content" id="chatContent" style={{ backgroundImage: `url(${user?.background_image ? user.background_image : "https://res.cloudinary.com/dwfngrwoe/image/upload/v1758392790/default_hgh8gm.webp"})`}}>
+                            {chatName ? 
+                            <div className="chat-header">
+                                {/* Group modal button */}
+                                <button id="backBtn" onClick={backToChats}>←</button>
+                                <div type="button" data-bs-toggle="modal" data-bs-target="#groupModal" onClick={getmembers}>
+                                    <img src={chatImg} alt="Profile" id="chatHeaderImg" />
+                                    <span id="chatName">{chatName}</span>
                                 </div>
                             </div>
                             :
-                            <div className="message received">
-                                {message.sender.profile ? (
-                                    <img src={message.sender.profile} className="avatar" />
-                                ) : (
-                                    <PersonCircle size={35} />
-                                )}
-                                <div className="message-bubble">
-                                    <div className="message-header">
-                                        <span className="nickname">{message?.sender.nickname}</span>
-                                        <span className="time">{prettyDate(message?.created_at)}</span>
-                                    </div>
-                                    <div className="message-content">
-                                        {message?.content}
-                                    </div>
-                                </div>
+                            <div/>
+                            }
+                            <div className="messages">
+                                {liveMessage?.map((message) => (
+                                    user.id === message.sender.id ?
+                                        <div className="message sent">
+                                            {message.sender.profile ? (
+                                                <a role="button" data-bs-toggle="modal" data-bs-target={`#${message.sender.id}`}>
+                                                    <img src={message.sender.profile} className="avatar"   />
+                                                </a>
+                                            ) : (
+                                                <a role="button" data-bs-toggle="modal" data-bs-target={`#${message.sender.id}`}>
+                                                    <PersonCircle className="avatar" />
+                                                </a>
+                                            )}
+                                            <div className="message-bubble">
+                                                <div className="message-header">
+                                                    <span className="nickname">{message?.sender.nickname}</span>
+                                                    <span className="time">{prettyDate(message?.created_at)}</span>
+                                                </div>
+                                                <div className="message-content">
+                                                    {message?.content}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        :
+                                        <div className="message received">
+                                            {message.sender.profile ? (
+                                                <img src={message.sender.profile} className="avatar" />
+                                            ) : (
+                                                <PersonCircle size={35} />
+                                            )}
+                                            <div className="message-bubble">
+                                                <div className="message-header">
+                                                    <span className="nickname">{message?.sender.nickname}</span>
+                                                    <span className="time">{prettyDate(message?.created_at)}</span>
+                                                </div>
+                                                <div className="message-content">
+                                                    {message?.content}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))}
+                                    <div ref={messagesEndRef} />
                             </div>
-                        ))}
-                        <div ref={messagesEndRef} />
-                </div>
 
-                <form className="chat-input" onSubmit={(e) => {
-                        e.preventDefault();
-                        sendMessage();
-                        setContent("");
-                        }}>
-                    <textarea
-                            placeholder="Type a message..."
-                            value={content} 
-                            onChange={(e) => setContent(e.target.value)}
-                        >
-                    </textarea>
-                    <button type="submit" class="send-icon-button">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="25" height="25" fill="currentColor" class="bi bi-send send-icon">
-                            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"></path>
-                        </svg>
-                    </button>
-                </form>
-                
-            </div>
-        </div>
+                            <form className="chat-input" onSubmit={(e) => {
+                                    e.preventDefault();
+                                    sendMessage();
+                                    setContent("");
+                                    }}>
+                                <textarea
+                                        placeholder="Type a message..."
+                                        value={content} 
+                                        onChange={(e) => setContent(e.target.value)}
+                                    >
+                                </textarea>
+                                <button type="submit" className="send-icon-button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="25" height="25" fill="currentColor" className="bi bi-send send-icon">
+                                        <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"></path>
+                                    </svg>
+                                </button>
+                            </form>
+                            
+                        </div>
+                    </div>
+                </>
+            )}
+        </>
     );
 }
 
